2d9567990acc476d5d2463ab35ea078949231cb7
ThanakritKK <xseigh.xc@gmail.com>
2026-02-17 22:43:18 -0500

    add speed-total length

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -33,6 +33,8 @@ function startTest() {
                 return word !== "";
             }).length;
 
+            var typedLength = userTypedText.length
+
             var wpm = 0; // Default value
 
             if (timeElapsed !== 0 && !isNaN(typedWords)) {
@@ -42,6 +44,7 @@ function startTest() {
             // Display the results
             var outputDiv = document.getElementById("output");
             outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+                "<p>Total Length: " + typedLength + "</p>" +
                 "<p>Words Typed: " + typedWords + "</p>" +
                 "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
                 "<p>Words Per Minute (WPM): " + wpm + "</p>";
